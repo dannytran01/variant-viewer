29058e51dafc6ec4a4c8b5cfe7d0da8e5c62926e
Danny Tran <dtran@adaptivebiotech.com>
2018-11-01 20:26:11 -0700

    Initial commit

---
+++ project/FrontendCommands.scala
@@ -0,0 +1,7 @@
+object FrontendCommands {
+  val dependencyInstall: String = "npm install"
+  val test: String = "npm run test-no-watch"
+  val serve: String = "npm run start"
+  val build: String = "npm run build-prod"
+  val buildDev: String = "npm run build-dev"
+}
